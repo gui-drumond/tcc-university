5bf119c400ce4b0213cf2673a2a903d4752fd19f
Guilherme Drumond <dev.drumond@gmail.com>
2024-11-23 16:51:37 -0300

    feat: separated component about

---
+++ src/app/components/sectionMiddle/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+      <div className="my-4 mx-32 py-16 ">
+        <h3 className="text-gray-90000 font-semibold text-4xl font-poppins mb-3">
+          Como funciona?
+        </h3>
+        <p className="text-xl ">
+          Você escolhe como será a utilizado o computador e nós devolveremos o
+          melhor custo-benefício, detalhando modelos e as peças. <br /> <br />
+          Essas informações serão listadas abaixo:
+        </p>
+      </div>
+    );
+}
