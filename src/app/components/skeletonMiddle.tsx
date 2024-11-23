5129ad11156bab63c4d4a81348ff7561f45e1b07
Guilherme Drumond <dev.drumond@gmail.com>
2024-11-23 16:51:49 -0300

    create skeleton about

--- src/app/components/skeletonMiddle.tsx
+++ src/app/components/skeletonMiddle.tsx
@@ -1,17 +1,15 @@
 export default function SkeletonMiddle(){
 
     return (
-      <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-        <div className="animate-pulse flex space-x-4">
-          <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-2 bg-slate-700 rounded"></div>
+      <div className="border border-blue-300 shadow-lg rounded-md p-14  w-[98%]">
+        <div className="animate-pulse flex space-x-4 mt-4">
+          <div className="flex-1 space-y-8 py-1">
             <div className="space-y-3">
-              <div className="grid grid-cols-3 gap-4">
-                <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+              <div className="grid grid-rows-3 gap-4 ml-10">
+                <div className="w-64 h-8 bg-slate-500 rounded row-span-1"></div>
+                <div className="w-3/4 h-4 bg-slate-500 rounded row-span-2"></div>
+                <div className="w-1/5 h-4 bg-slate-500 rounded row-span-1"></div>
               </div>
-              <div className="h-2 bg-slate-700 rounded"></div>
             </div>
           </div>
         </div>
