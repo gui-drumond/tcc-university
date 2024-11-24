a63d68f28e7f5bffe5983bd87180d2a1746eb7cb
Guilherme Drumond <dev.drumond@gmail.com>
2024-11-23 23:19:10 -0300

    feat: make a skeleton

--- src/app/components/skeletonMiddle.tsx
+++ src/app/components/skeletonMiddle.tsx
@@ -1,18 +1,9 @@
-export default function SkeletonMiddle(){
-
-    return (
-      <div className="border border-blue-300 shadow-lg rounded-md p-14  w-[98%]">
-        <div className="animate-pulse flex space-x-4 mt-4">
-          <div className="flex-1 space-y-8 py-1">
-            <div className="space-y-3">
-              <div className="grid grid-rows-3 gap-4 ml-10">
-                <div className="w-64 h-8 bg-slate-500 rounded row-span-1"></div>
-                <div className="w-3/4 h-4 bg-slate-500 rounded row-span-2"></div>
-                <div className="w-1/5 h-4 bg-slate-500 rounded row-span-1"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+export default function SkeletonMiddle() {
+  return (
+    <div className="flex rounded-2xl w-[45%] max-w-2xl h-[156px] align-middle mt-10 cursor-pointer animate-pulse  bg-slate-400 shadow-2xl  z-0">
+      <div className="grid grid-rows-3 gap-4 ml-10">
+        <div className="w-64 h-[156px] rounded row-span-1"></div>
       </div>
-    );
+    </div>
+  );
 }
